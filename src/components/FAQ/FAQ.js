1fd59817fc98c3dd0efc36aefd9b86223039f314
Tomasz Szabla <111thomas1111@gmail.com>
2020-09-06 21:53:35 +0200

    Add FAQ

--- src/components/FAQ/FAQ.js
+++ src/components/FAQ/FAQ.js
@@ -1,5 +1,6 @@
 import React from 'react';
-
+import styles from './FAQ.scss';
+import {Link} from 'react-router-dom';
 import Container from '../Container/Container';
 import Hero from '../Hero/Hero';
 import PropTypes from 'prop-types';
@@ -9,10 +10,12 @@ const FAQ = () => (
   <Container>
     <Hero titleText={faqContents.title} image={listData.image} />
     <h2>{infoContents.subtitle}</h2>
+    <div className={styles.component}>
+      <p><Link exact to='/' activeClassName='active'>{faqContents.question1}</Link></p>
+      <p><Link exact to='/' activeClassName='active'>{faqContents.question2}</Link></p>
+      <p><Link exact to='/' activeClassName='active'>{faqContents.question3}</Link></p>
+    </div>
     
-    <p>{faqContents.question1}</p>
-    <p>{faqContents.question2}</p>
-    <p>{faqContents.question3}</p>
   </Container>
 );
   
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import {NavLink, Link} from 'react-router-dom';
 import styles from './Header.scss';
+import {NavLink, Link} from 'react-router-dom';
+
 import Container from '../Container/Container';
 import Icon from '../Icon/Icon';
 
@@ -8,7 +9,7 @@ import Icon from '../Icon/Icon';
 class Header extends React.Component {
   render(){
     return (
-      <header className={styles.Component}>
+      <header className={styles.component}>
         <Container>
           <div className={styles.wraper}>
             <Link to='/' className={styles.logo}>
